5b07aba6b0c512d57419490b99ecb095ec0b00e8
royrockon <dev@sqyd.studio>
2026-04-13 16:49:25 -0400

    Got my weighted dice algorithm in!

--- rust_dice/src/die.rs
+++ rust_dice/src/die.rs
@@ -1,5 +1,6 @@
 use std::u32;
 
+
 struct InternalRng {
     seed: u64,
 }
@@ -27,28 +28,55 @@ impl InternalRng {
 
     pub fn get_rng(&mut self) -> u32 {
         self.next() as u32
+    }
+
+    pub fn get_number(&mut self, max: u32) -> u32{
+        let seed = self.next();
+        (seed % max) + 1 as u32
     }    
 }
 
 pub struct Die {
-    faces: u32,
     current_face: u32,
+    face_weights: Vec<u32>,
+    total_weight: u32,
     internal_rng: InternalRng,
 }
 
 impl Die{
-    pub fn new(faces: u32, seed: u64) -> Self {
-        Die { faces, current_face: 1, internal_rng: InternalRng::new(seed) }
+    pub fn new(faces: u32, seed: u64, std_weight: u32, weight_varience: u32) -> Self {
+        let face_weights = map_face_weights(seed, faces, std_weight, weight_varience);
+        Die { 
+            current_face: 1,
+            face_weights : face_weights.0,
+            total_weight : face_weights.1,
+            internal_rng: InternalRng::new(seed)
+        }
     }
 
     pub fn roll(&mut self) -> u32 {
-        let rng = self.internal_rng.get_rng();
-        self.current_face = rng % self.faces + 1;
+        let random_number = self.internal_rng.get_number(self.total_weight);
+        for i in 0..self.face_weights.len(){
+            if random_number <= self.face_weights[i] {
+                self.current_face = (i + 1) as u32;
+                break;
+            } 
+        }
         self.current_face
     }
 }
 
+fn map_face_weights(seed: u64, face_count: u32, std_weight: u32, weight_varience: u32) -> (Vec<u32>, u32){
+    let mut rng = InternalRng::new(seed);
+    let mut weights = Vec::new();
+    let mut cumulative = 0;
+    for _ in 0..face_count{
+        cumulative += (std_weight + rng.get_number(weight_varience));
+        weights.push(cumulative);
+    }
 
+    (weights, cumulative)
+}
 
 #[cfg(test)]
 
@@ -56,7 +84,7 @@ impl Die{
 fn randomize(){
     let mut rng = InternalRng::new(57);
     let mut counts: [i32; 11] = [0,0,0,0,0,0,0,0,0,0,0];
-    for i in 0..10000 {
+    for i in 0..1000 {
         let number = rng.get_rng();
         let digitcount = number.to_string().len();
         counts[digitcount] += 1;
@@ -68,14 +96,29 @@ fn randomize(){
 }
 
 #[test]
+fn random_number(){
+    let mut rng = InternalRng::new(222);
+    
+    println!("Getting random numbers 1 - 6");
+    for _ in 0..100{
+        println!("{}", rng.get_number(6));
+    }
+
+    println!("Getting random numbers 1 - 20");
+    for _ in 0..100{
+        println!("{}", rng.get_number(20));
+    }
+}
+
+#[test]
 fn die_test(){
-    let faces = 4;
-    let mut die = Die::new(faces, 44);
+    let faces = 10;
+    let mut die = Die::new(faces, 1789, 100, 100);
     let mut counts: Vec<i32> = vec![0; faces as usize];
     for i in 0..1000000 {
         let roll = die.roll();
         counts[(roll - 1) as usize] += 1;
-        println!("{} --- {}", i, die.roll())
+        println!("{} --- {}", i, roll);
     }
 
     let sum_of_counts = {
@@ -86,16 +129,12 @@ fn die_test(){
         sum
     };
 
-    println!("Sum of counts = {}", sum_of_counts);
-    assert_eq!(sum_of_counts, 1000000);
-
     let mut percents: Vec<f64> = vec![0.0; faces as usize];
     let mut sum_of_percents = 0.0; 
     for i in 0..faces {
         percents[i as usize] = (counts[i as usize] as f64 / sum_of_counts as f64) * 100.0;
         sum_of_percents += percents[i as usize];
-        println!("Face of {} count of {}", i + 1, counts[i as usize]);
-        println!("Precent of total {}", percents[i as usize]);
+        println!("Face of {} count of {} percent of {}", i + 1, counts[i as usize], percents[i as usize]);
     }
 
     let mean = sum_of_percents as f64 / counts.len() as f64;
@@ -111,15 +150,9 @@ fn die_test(){
 
 
 #[test]
-fn max_u32(){
-    let digits = u32::MAX.to_string().len();
-    println!("{} Digitcount: {}", u32::MAX, digits);
-}
-
-#[test]
-fn min_u32(){
-    let digits = u32::MIN.to_string().len();
-    println!("{} Digitcount: {}", u32::MIN, digits);
+fn face_weights_mapping(){
+    let die = Die::new(10, 1, 100, 10);
+    println!{"Face weights = {:?} Total = {}", die.face_weights, die.total_weight};
 }
 
 
