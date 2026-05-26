4d6bca039bbe7da8a25045ee4db6c3fc52c3a7b2
royrockon <dev@sqyd.studio>
2026-05-26 18:41:44 -0400

    converting to typescript will help in the long run.

--- dice_tray_web/src/App.tsx
+++ dice_tray_web/src/App.tsx
@@ -1,8 +1,8 @@
 import { useState, useEffect } from 'react'
 import init, { DiceAllocatorHandle } from '../pkg/dice_wasm';
-import DiceBag from './Components/DiceBag';
+import { createBox, motion } from "motion/react"
 
-import './App.css'
+import './App.css';
 
 function App() {
   const [wasmReady, setWasmReady] = useState(false);
@@ -17,7 +17,6 @@ function App() {
         setAppHandle(handle);
       } catch (error) {
         console.error("Failed to initialize WASM:", error);
-        setGreeting("Failed to load WASM");
       }
     };
     
@@ -32,13 +31,32 @@ function App() {
       </div>
     )
   }
-
-  return (
-   <div className="board">
-    <h1>Dice Tray!</h1>
-    <DiceBag appHandle={appHandle}></DiceBag>
-   </div>
-  )
+  else{
+    return (
+      <div className="board">
+        <h1>Dice Tray!</h1>
+        <div className="tray">
+          <motion.div
+            style={box}
+            whileHover={{scale: 1.2}}
+            whileTap={{scale:0.8}}
+            initial={{opacity: 0, scale: 0}}
+            animate={{rotate: 360, scale: 1, opacity: 1}}
+            transition={{duration: 1, repeat: 0}}
+          />
+        </div>
+      </div>
+    )
+  }
 }
+
+const box = {
+    align: "center",
+    width: 100,
+    height: 100,
+    backgroundColor: "red",
+    borderRadius: 5,
+}
+ 
 
 export default App
